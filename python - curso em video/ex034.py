960b12bd566a23ff6fc379ec1bf2df59a8548e11
Willian Nakau <wii.nakau@gmail.com>
2019-09-15 20:15:15 -0300

    exercicios resolvidos

--- python - curso em video/ex034.py
+++ python - curso em video/ex034.py
@@ -8,3 +8,12 @@ calcule um aumento de 10%
 
 Para os inferiores ou iguais, o aumento é de 15%.
 '''
+
+salario = float(input('Qual o salário do funcionário? '))
+
+if salario > 1250:
+    aumento = salario + (salario * 10/100)
+else:
+    aumento = salario + (salario * 15/100)
+
+print('Quem ganhava R${:.2f} passa a ganhar R${:.2f}.' .format(salario, aumento))
